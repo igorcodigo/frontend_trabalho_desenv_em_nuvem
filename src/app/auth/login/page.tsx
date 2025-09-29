a817045ea4265e8d3b578d3ce16527f6581c5ebd
Beatrizcop <anabeatriz2725bnb@gmail.com>
2025-09-29 14:09:09 -0300

    Update page.tsx - Melhoria do design pagina de login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,11 @@ import { LoginForm } from "./_components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="flex min-h-[calc(100vh-72px)] flex-col items-center justify-center bg-gradient-to-br from-blue-400 to-purple-600">
+      <div className="mb-8 text-center">
+        <h1 className="text-5xl font-bold text-white">Bem-vindo à sua Lista de Tarefas</h1>
+        <p className="mt-2 text-lg text-white/80">Organize seu dia, conquiste seus objetivos.</p>
+      </div>
       <LoginForm />
     </div>
   );
